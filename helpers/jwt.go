b8dab010c6d823bed3197464c1409a60e47f4a3a
mhdiiilham <muhd.iiilham@gmail.com>
2020-06-16 19:59:04 +0700

    Created func verifyToken at helpers/jwt.go

--- helpers/jwt.go
+++ helpers/jwt.go
@@ -18,3 +18,17 @@ func CreateJWTToken(id uint, email string) (string, error) {
 
 	return at.SignedString([]byte("HelloWorld123"))
 }
+
+func verifyToken(ht string) (*jwt.Token, error) {
+	token, err := jwt.Parse(ht, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", ok)
+		}
+		return []byte(os.Getenv("SECRET")), nil
+	})
+	if err != nil{
+		return nil, err
+	}
+
+	return token, nil
+}

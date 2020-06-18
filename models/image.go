308e1e6b289d4ca41925dfc9eb3dfdc6abd3d099
mhdiiilham <muhd.iiilham@gmail.com>
2020-06-18 22:53:14 +0700

    add `gorm.Model` at line 8 modified:   models/image.go

--- models/image.go
+++ models/image.go
@@ -5,6 +5,7 @@ import "github.com/jinzhu/gorm"
 
 // Image model
 type Image struct {
+	gorm.Model
 	URL string `json:"url" gorm:"type:varchar(50)"`
 	UserID string `json:"url" gorm:"type:varchar(10)"`
 	DeleteHash string `json:"delete_has"`

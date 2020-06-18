c335969330a223eb5440b4017f4f4032ea7fc446
mhdiiilham <muhd.iiilham@gmail.com>
2020-06-18 22:40:43 +0700

    Create image's model

---
+++ models/image.go
@@ -0,0 +1,12 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+
+// Image model
+type Image struct {
+	URL string `json:"url" gorm:"type:varchar(50)"`
+	UserID string `json:"url" gorm:"type:varchar(10)"`
+	DeleteHash string `json:"delete_has"`
+	ImgurID string `json:"imgur_id" gorm:"type:varchar(10)"`
+}

be8dc857144bc4640119ee7747126626b14a1a58
mhdiiilham <muhd.iiilham@gmail.com>
2020-06-19 20:52:40 +0700

    remove import `net/http` and logarus at main.go
    run server using c.Run()

--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	db "github.com/mhdiiilham/ginorm/db"
 	"github.com/mhdiiilham/ginorm/routers"
-	log "github.com/sirupsen/logrus"
-	"net/http"
 )
 
 func main() {
-	port := ":8000"
+	const PORT = ":8000"
 	router := routers.Router()
 	db.MySQL()
-	log.Fatal(http.ListenAndServe(port, router))
+	router.Run(PORT)
 }
